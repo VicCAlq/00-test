5fceb72c676d005328f40782f908cbc042122dad
Victor Cavalcanti <87677160+VicMCA@users.noreply.github.com>
2026-06-08 12:44:55 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,13 @@
 import { configDefaults, defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+/*
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
   },
+*/
 })
